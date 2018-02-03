8b80fc5aac4f25b94a0876c90aa674ec4e6f3fe9
icecap <jeryliketom@gmail.com>
2018-02-02 21:25:27 -0500

    implemented RMSprop and Aadam

--- CS231n-assignment2/cs231n/optim.py
+++ CS231n-assignment2/cs231n/optim.py
@@ -100,7 +100,11 @@ def rmsprop(x, dx, config=None):
     # in the next_x variable. Don't forget to update cache value stored in    #
     # config['cache'].                                                        #
     ###########################################################################
-    pass
+    config['cache'] = config['cache'] * config['decay_rate'] \
+                                        + (1 - config['decay_rate']) * dx**2
+    next_x = x - config['learning_rate'] * dx \
+                                / (np.sqrt(config['cache']) + config['epsilon'])
+
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -137,7 +141,18 @@ def adam(x, dx, config=None):
     # the next_x variable. Don't forget to update the m, v, and t variables   #
     # stored in config.                                                       #
     ###########################################################################
-    pass
+    learning_rate = config['learning_rate']
+    beta1 = config['beta1']
+    beta2 = config['beta2']
+    epsilon = config['epsilon']
+
+    # Value after the update
+    config['t'] += 1
+    config['m'] = beta1 * config['m'] + (1 - beta1) * dx
+    config['v'] = beta2 * config['v'] + (1 - beta2) * dx**2
+    mt_hat = config['m'] / (1 - (beta1)**config['t'])
+    vt_hat = config['v'] / (1 - (beta2)**config['t'])
+    next_x = x - learning_rate * mt_hat / (np.sqrt(vt_hat + epsilon))
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
